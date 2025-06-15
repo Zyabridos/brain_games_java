0bed005cf2e2b8eedfca90839e7eb856ffe309b1
Nina Zyabrina <zyabrina95@gmail.com>
2025-06-15 21:28:55 +0200

    fix lint

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
 run-dist:
 	cd app && ./gradlew clean installDist && ./build/install/app/bin/app
+
+install-dist:
+	cd app && ./gradlew installDist
+
+clean:
+	cd app && ./gradlew clean
--- app/src/main/java/hexlet/code/Round.java
+++ app/src/main/java/hexlet/code/Round.java
@@ -4,12 +4,12 @@ public final class Round {
     private final String question;
     private final String correctAnswer;
 
-    public Round(String question, String correctAnswer) {
-        this.question = question;
-        this.correctAnswer = correctAnswer;
+    public Round(String questionText, String correctAnswerText) {
+        this.question = questionText;
+        this.correctAnswer = correctAnswerText;
     }
 
-    public String getQuestion() {
+public String getQuestion() {
         return question;
     }
 
