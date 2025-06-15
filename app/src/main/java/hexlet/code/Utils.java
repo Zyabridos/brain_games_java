4b5225910789642ed82215e239961d9b141c486b
Nina Zyabrina <zyabrina95@gmail.com>
2025-06-15 20:48:12 +0200

    code refacroting

--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -1,8 +1,11 @@
 package hexlet.code;
 
+import java.util.Random;
+
 public class Utils {
+    private static final Random RANDOM = new Random();
+
     public static int generateRandomNumber(int max) {
-        double randomNumber = Math.random();
-        return (int) Math.round(randomNumber * max);
+        return RANDOM.nextInt(max + 1);
     }
 }
--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -1,8 +1,8 @@
 package hexlet.code.games;
 
+import hexlet.code.Engine;
 import hexlet.code.Round;
 import hexlet.code.Utils;
-import hexlet.code.Engine;
 
 public class Calc {
     public static String getRules() {
@@ -11,10 +11,22 @@ public class Calc {
 
     public static Round[] prepareRounds() {
         Round[] rounds = new Round[Engine.getRoundsCount()];
+
         for (int i = 0; i < rounds.length; i++) {
-            int first = Utils.generateRandomNumber(50);
-            int second = Utils.generateRandomNumber(50);
             String operator = generateOperator();
+            int first;
+            int second;
+
+            if (operator.equals("/")) {
+                do {
+                    second = Utils.generateRandomNumber(1, 10); // не 0
+                    int result = Utils.generateRandomNumber(2, 10);
+                    first = result * second; // гарантирует целое деление
+                } while (second == 0);
+            } else {
+                first = Utils.generateRandomNumber(0, 50);
+                second = Utils.generateRandomNumber(0, 50);
+            }
 
             String question = first + " " + operator + " " + second;
             int result;
@@ -27,13 +39,15 @@ public class Calc {
                 default -> throw new IllegalStateException("Unexpected operator: " + operator);
             }
 
-            rounds[i] = new Round(question, Integer.toString(result));
+            String correctAnswer = Integer.toString(result);
+            rounds[i] = new Round(question, correctAnswer);
         }
+
         return rounds;
     }
 
     private static String generateOperator() {
-        int n = Utils.generateRandomNumber(3); // 0–3
+        int n = Utils.generateRandomNumber(0, 3); // 0–3
         return switch (n) {
             case 0 -> "+";
             case 1 -> "-";
